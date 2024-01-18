41c0d758c551ae6193131833294ff9d6a82b316f
MohamedElderkaoui <mderkaoui10@gmail.com>
2024-01-18 09:22:50 +0100

    wsd

--- hello_wolrd/hello_wolrd/models.py
+++ hello_wolrd/hello_wolrd/models.py
@@ -35,7 +35,7 @@ class Book_author(rx.Model, table=True):
     book_id: int= sqlmodel.Field(foreign_key="book.id")
     author_id: int= sqlmodel.Field(foreign_key="author.id")
 
-class queryAuthor(rx.State):
+class query(rx.State):
     bookSta:list(Book)
     authorSta:list(Author)
     book_authorState:list(Book_author)
@@ -96,4 +96,88 @@ class queryAuthor(rx.State):
             session.query(Author).filter(Author.id == id).delete()
             session.commit()
     
+def book_form():
+    return rx.Vstack(
+        rx.Label('Title'),
+        rx.form(
+            rx.TextInput("title"),
+            # registdry
+            rx.TextInput(
+                "registry",
+                placeholder="YYYY"
+                
+            ),
+            rx.Checkbox(
+                value=["english"],
+                label="English"
+                ),
+            rx.Button(
+                'Submit',
+                on_click=lambda _: book().add_book(rx.get('title'),
+                                                    registdry=int(rx.get('registry')),
+                                                    english=(bool)(rx.get('english'))[0])
+                )
+        )
+    )
+# import DBManager
+import DBManager
+def get_db():#hello_wolrd\reflex.db
+    db = DBManager.DBManager()
+    @db.route('/books/<id>')
+    class BookHandler(rx.View):
+        def init(self):
+            self.book = None
+            self.error = ""
+            self.load_data(id)
+            def load_data(self, id):
+                try:
+                    self.book = db.get_book(id)
+                except Exception as e:
+                    pass
+                # print(e)
+                if not self.book:
+                    self.error = f'No book with ID {id}'
+                else:
+                    
+    
+class book:
+    @staticmethod
+    def show_books(books: List[Book]):
+        if not books:
+            return rx.Label("No Books Found")
+        else:
+            return rx.Table(
+                headers=['ID', 'Title', 'registdry'],
+                rows=[(b.id, b.title, str(b.registdry)) for b
+                      in sorted(books, key=lambda x: (not x.is_english
+                                                      ,-x.registdry))]
+                ).set_style(border='1px solid black')
+    @staticmethod
+    def add_book(title:str, registdry:str):
+        db = get_db()
+        with db.session.begin(subtransactions=True) as session:
+            new_book = Book(title=title, registdry=registdry)
+            session.add(new_book)
+            
         
+        
+        
+            
+                
+from hello_wolrd import app
+@app.route('/books/<int:book_id>/authors/add')
+async def add_author_to_book(request, book_id):
+    return app.response.html(
+        rx.Page(
+            title='Add Author to Book',
+            head=[],
+            body=rx.HStack([
+                rx.VStack([
+                    rx.Link("/books", 'Back to Books'),
+                    rx.Heading('Add an author to this book'),
+                    ]),
+                rx.Spacer(),
+                book_form()
+                ])
+         ) )
+@app.route('/books/<int:book_id>')
